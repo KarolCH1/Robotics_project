332878fa1b14c05b3e174b9c614390f442786b09
KarolCH1 <36267018+KarolCH1@users.noreply.github.com>
2024-11-05 15:46:34 +0100

    commit05_11_2024

---
+++ robot.py
@@ -0,0 +1,33 @@
+import dynamixel_sdk as dxl #git clone https://github.com/ROBOTIS-GIT/DynamixelSDK.git
+import cv2 as cv
+
+ADDR_MX_TORQUE_ENABLE = 24
+ADDR_MX_CW_COMPLIANCE_MARGIN = 26
+ADDR_MX_CCW_COMPLIANCE_MARGIN = 27
+ADDR_MX_CW_COMPLIANCE_SLOPE = 28
+ADDR_MX_CCW_COMPLIANCE_SLOPE = 29
+ADDR_MX_GOAL_POSITION = 30
+ADDR_MX_MOVING_SPEED = 32
+ADDR_MX_PRESENT_POSITION = 36
+ADDR_MX_PUNCH = 48
+PROTOCOL_VERSION = 1.0
+DXL_IDS = [1,2,3,4]
+DEVICENAME = 'COM11' #USB11
+BAUDRATE = 1000000
+TORQUE_ENABLE = 1
+TORQUE_DISABLE = 0
+portHandler = dxl.PortHandler(DEVICENAME)
+packetHandler = dxl.PacketHandler(PROTOCOL_VERSION)
+portHandler.openPort()
+portHandler.setBaudRate(BAUDRATE)
+
+for DXL_ID in DXL_IDS:
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_MARGIN, 0)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_MARGIN, 0)
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_SLOPE, 32)
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_SLOPE, 32)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_MOVING_SPEED, 10)
+    
+
+
